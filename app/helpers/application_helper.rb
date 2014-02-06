03214a78926ec8a508cced371b1e9a2c82bb499b
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2014-02-06 14:14:38 -0600

    [Finishes #65322800 ] Design for home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def errors_on_resource_for_field(resource, field)
     unless resource.errors[field].empty?
-      content_tag :div, resource.errors[field].join(', ').to_s, class: 'errors-form'
+      content_tag :div, resource.errors[field].join('<br/> ').html_safe, class: 'errors-form'
     end
   end
 
